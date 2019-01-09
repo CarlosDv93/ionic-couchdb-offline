31f710b26e448f4bce4878e53fa464d9274f8345
Carlos David A Arcos <carlos9_cv@hotmail.com>
2019-01-09 15:49:23 -0200

    Aula 8 - Geolocalização

--- aula4/src/pages/home/home.ts
+++ aula4/src/pages/home/home.ts
@@ -1,17 +1,24 @@
+import { User } from './../../entity/user';
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { UserProvider } from '../../providers/user/user';
+import { Geolocation } from '@ionic-native/geolocation';
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
+  providers: [
+    Geolocation
+  ]
 })
 export class HomePage {
 
   public users: any;
-  public usuario= {name:"",age:""};
+  public usuario= new User();
 
-  constructor(public navCtrl: NavController, public usuarioService: UserProvider) {
+  constructor(public navCtrl: NavController, 
+    public usuarioService: UserProvider,
+    public geolocation: Geolocation) {
 
   }
 
@@ -32,7 +39,16 @@ export class HomePage {
   }
 
   public salvarUsuario() {
+    this.geolocation.getCurrentPosition().then((resp) => {
+      // resp.coords.latitude
+      // resp.coords.longitude
+      this.usuario.latitude = resp.coords.latitude;
+      this.usuario.longitude = resp.coords.longitude;
       this.usuarioService.creteUser(this.usuario);
+     }).catch((error) => {
+       console.log('Error getting location', error);
+     });
+     
   };
 
   public editarUsuario(usuario){
