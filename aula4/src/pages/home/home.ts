3dfd179aba696f60e502bc42b5d16ce1c9162f8f
Carlos David A Arcos <carlos9_cv@hotmail.com>
2019-01-09 17:00:35 -0200

    Aula 10

--- aula4/src/pages/home/home.ts
+++ aula4/src/pages/home/home.ts
@@ -4,13 +4,15 @@ import { NavController } from 'ionic-angular';
 import { UserProvider } from '../../providers/user/user';
 import { Geolocation } from '@ionic-native/geolocation';
 import { Camera, CameraOptions } from '@ionic-native/camera';
+import { Dialogs } from '@ionic-native/dialogs';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html',
   providers: [
     Geolocation,
-    Camera
+    Camera,
+    Dialogs
   ]
 })
 export class HomePage {
@@ -27,7 +29,8 @@ export class HomePage {
   constructor(public navCtrl: NavController, 
     public usuarioService: UserProvider,
     public geolocation: Geolocation,
-    private camera: Camera) {
+    private camera: Camera,
+    private dialogs: Dialogs) {
 
   }
 
@@ -54,6 +57,7 @@ export class HomePage {
       this.usuario.latitude = resp.coords.latitude;
       this.usuario.longitude = resp.coords.longitude;
       this.usuarioService.creteUser(this.usuario);
+      this.dialogs.alert('Usuário Salvo com sucesso')
      }).catch((error) => {
        console.log('Error getting location', error);
      });
@@ -74,7 +78,10 @@ export class HomePage {
      // If it's base64 (DATA_URL):
      let base64Image = 'data:image/jpeg;base64,' + imageData;
      this.usuario.foto = base64Image;
-     console.log("Upload da imagem feito com sucesso!");
+     this.dialogs.alert('Upload feito com sucesso')
+     .then(() => console.log('Dialog dismissed'))
+     .catch(e => console.log('Error displaying dialog', e));
+
     }, (err) => {
      // Handle error
      console.log(err);
